0443db4069663aa94a9c420022de2bbb9ac36101
marakul89-oss <marakul89@gmail.com>
2026-02-25 23:24:11 +0500

    Add files via upload

---
+++ my_bot.py
@@ -0,0 +1,43 @@
+
+import telebot
+from telebot import types
+
+# Вставь сюда свой API токен, который дал BotFather
+bot = telebot.TeleBot('8311827359:AAHViPd8fAk0hRMVmmHJNtro4VxhlmHf2_4')
+
+# Команда /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Создаем клавиатуру с кнопками
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)  # Создаем клавиатуру
+    item1 = types.KeyboardButton("Цена")  # Кнопка для прайса
+    item2 = types.KeyboardButton("Договор оферты")  # Кнопка для договора
+    item3 = types.KeyboardButton("Обратная связь")  # Кнопка для обратной связи
+
+    markup.add(item1, item2, item3)  # Добавляем кнопки на клавиатуру
+
+    # Отправляем сообщение с кнопками
+    bot.send_message(
+        message.chat.id,
+        "Привет! Добро пожаловать в бот!\n\n"
+        "Этот бот научит тебя создавать и монетизировать AI-аватара с нуля!",
+        reply_markup=markup
+    )
+
+# Обработчик кнопки "Цена"
+@bot.message_handler(func=lambda message: message.text == "Цена")
+def price(message):
+    bot.send_message(message.chat.id, "Цена за гайд: 690 рублей.")
+
+# Обработчик кнопки "Договор оферты"
+@bot.message_handler(func=lambda message: message.text == "Договор оферты")
+def offer(message):
+    bot.send_message(message.chat.id, "https://docs.google.com/document/d/17eEBGv6G-LzUcZpyvygfLzJT-W_BloOs/edit?usp=sharing&ouid=107708342823288631650&rtpof=true&sd=true")
+
+# Обработчик кнопки "Обратная связь"
+@bot.message_handler(func=lambda message: message.text == "Обратная связь")
+def contact(message):
+    bot.send_message(message.chat.id, "Mara102@mail.ru")
+
+# Запускаем бота
+bot.polling()
